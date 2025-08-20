a36c696303b2d118e1a40c99ab0e2fdac6f87b04
Hamza Belabbes <hamza.belabbes@outlook.com>
2025-08-20 21:35:30 +0200

    Initial commit with whole Terraform project

---
+++ README.md
@@ -0,0 +1,12 @@
+## Part 2 – Decoupled Architecture for Batch Ingestion  
+
+![Pipeline Architecture](Arcdhitecture%20Ingestion%20V2.png)
+
+
+In the first version, the architecture was fully functional but tightly coupled: every uploaded file could trigger a Glue job directly.  
+
+In this second version, the architecture is restructured to decouple ingestion from processing. Instead of running a Glue job for each single file (and nobody really wants a Spark job billed just to process one file), files are now collected and processed in batch.  
+
+This design is more cost-efficient, scalable, and aligned with real-world ingestion patterns.  
+
+This batch job is managed through the WorkflowMetadata table : for each prefix, we’ll have a Step Function, along with a batchSize column that we can control to decide how many files are required before the pipeline kicks off.
---
+++ Terraform/main.tf
@@ -0,0 +1,52 @@
+provider "aws" {
+  region = "eu-west-3"
+}
+
+module "Dynamodb" {
+  source = "./DynamoDb"
+}
+
+module "S3Bucket" {
+  source                  = "./S3Bucket"
+  preprocess_bucket_name  = "preworkflow-ingestion-bucket"
+  postprocess_bucket_name = "postworkflow-ingestion-bucket"
+  artifacts_bucket_name   = "gluejobs-ingestion-bucket"
+}
+
+module "Sqs" {
+  source                = "./Sqs"
+  preprocess_queue_name = "Preprocess_Queue"
+  preprocess_bucket_arn = module.S3Bucket.preprocess_bucket_arn
+}
+
+module "S3Notifications" {
+  source                 = "./S3Notifications"
+  preprocess_bucket_name = module.S3Bucket.preprocess_bucket_name
+  preprocess_queue_arn   = module.Sqs.preprocess_queue_arn
+  sqs_queue_policy_id    = module.Sqs.queue_policy_id
+}
+
+module "start-workflow-lambda" {
+  source            = "./Lambda/start-workflow-lambda"
+}
+
+module "end-workflow-lambda" {
+  source            = "./Lambda/end-workflow-lambda"
+}
+
+module "files_to_process_lambda" {
+  source            = "./Lambda/files_to_process_lambda"
+  aws_sqs_queue_arn = module.Sqs.preprocess_queue_arn
+
+}
+
+module "glue-job"{
+  source = "./Glue/ingestion-glue-job"
+  artifacts_bucket_name = module.S3Bucket.artifacts_bucket_name
+}
+
+module "stepfunctions" {
+  source ="./Stepfunction"
+  end_workflow_lambda_arn = module.end-workflow-lambda.end_workflow_lambda_arn
+  ingestion_glue_job_name = module.glue-job.ingestion_glue_job_name
+}
